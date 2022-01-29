74d71cdee2cad13496dd9b153c23d201ac22b7f7
Sera Zenginler <serazenginler@outlook.com>
2022-01-29 23:57:48 +0300

    Style changes

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,26 @@
-import { Layout } from "antd";
+import {
+	Col,
+	Layout,
+	Row,
+	Typography,
+} from "antd";
 import React, { FC } from "react";
 
+import heroImg from "../../assets/hero-image.svg";
+import style from "./style.module.scss";
+
 const Main:FC = () => {
-  const {Content} = Layout;
+  const { Content } = Layout;
+  const { Title } = Typography;
   return <Content>
-    <h1>Hi</h1>
+    <Row className={`${style.container} container centered`}>
+      <Col span={24}>
+        <Title level={1}>Fastest & secure platform to invest in crypto</Title>
+      </Col>
+      <Col span={24}>
+        <img src={heroImg} alt="Blockchain app" className={style.img}/>
+      </Col>
+    </Row>
   </Content>;
 };
 
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,17 @@
 import {
-	Button,
 	Layout,
 	Menu,
 } from "antd";
 import React, { FC } from "react";
 
-import { SyncOutlined } from "@ant-design/icons";
-
+import crappoLogo from "../../assets/logo.svg";
 import style from "./style.module.scss";
 
 const Navbar: FC = () => {
   const {Header} = Layout;
   return <Header style={{background: "transparent"}} className="container">
   <div className={`${style.logo} centered`}>
-  <SyncOutlined />
+  <img src={crappoLogo} alt="Crappo Logo" />
   </div>
   <Menu mode="horizontal" defaultSelectedKeys={['1']} className={style.container}>
     {new Array(5).fill(null).map((_, index) => {
